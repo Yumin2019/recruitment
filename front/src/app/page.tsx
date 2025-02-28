f8bce8d6b10c144ce8d91f67ed2e126ab5113511
Yumin Kim <49779139+Yumin2019@users.noreply.github.com>
2025-02-28 23:44:12 +0900

    feat: 서비스 로고 이미지 추가 feat: 메인페이지 상단 탭바, 스크롤바(진행중) chore: draggable, icons 라이브러리 추가

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,76 +1,107 @@
+"use client";
+
 import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
+import { IoIosNotificationsOutline, IoIosMenu } from "react-icons/io";
+import { MutableRefObject, useRef } from "react";
+import { useDraggable } from "react-use-draggable-scroll";
+
+const dataList = [
+  {
+    image: "/developer_logo.png",
+    title: "React Native Developer",
+    content:
+      "React Native Developer with experience in mobile application development.",
+  },
+  {
+    image: "/developer_logo.png",
+    title: "React Developer",
+    content: "React Developer with experience in web application development.",
+  },
+  {
+    image: "/developer_logo.png",
+    title: "Node.js Developer",
+    content: "Node.js Developer with experience in backend development.",
+  },
+  {
+    image: "/developer_logo.png",
+    title: "Python Developer",
+    content:
+      "Python Developer with experience in data analysis and machine learning.",
+  },
+  {
+    image: "/developer_logo.png",
+    title: "Java Developer",
+    content: "Java Developer with experience in backend development.",
+  },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // horizontal scrollbar
+  const ref = useRef<HTMLDivElement>(
+    undefined
+  ) as React.MutableRefObject<HTMLInputElement>;
+  const { events } = useDraggable(ref, {
+    applyRubberBandEffect: true,
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div>
+      {/* 상단 탭바 */}
+      <div className="flex bg-amber-600 absolute top-0 left-0 right-0 h-[56px] items-center p-[5px]">
+        <img src="/developer_logo_horizon.png" className="h-full" />
+        <div className="flex-1" />
+        <div className="flex" style={{ fontSize: 32, gap: 5 }}>
+          <CiSearch className="hover:bg-stone-50/50 rounded-full" />
+          <IoIosNotificationsOutline className="hover:bg-stone-50/50 rounded-full" />
+          <IoIosMenu className="hover:bg-stone-50/50 rounded-full" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+
+      <div className="mt-[56px]" />
+
+      {/* 공고 이미지 가로 리스트 */}
+      <div className="flex-col">
+        <p className="text-sms text-xl font-medium text-black mb-[5px]">
+          합격 가능성이 높은 포지션
+        </p>
+        <div
+          className="flex max-w space-x-3 overflow-x-scroll scrollbar-hide p-5 bg-amber-400"
+          {...events}
+          ref={ref}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
+          {dataList &&
+            dataList.map((v, index) => {
+              return (
+                // CSS는 정말 지랄 맞네... ;;
+                <img
+                  src={
+                    // v.image
+                    "https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fcompany%2F38576%2Fasqwioroxlcs4syg__1080_790.png&w=700&q=100"
+                  }
+                  className="h-[150px]  hover:scale-101 ease-in-out duration-300 rounded-md border-1 border-black/30"
+                />
+                // <div className="bg-amber-800">
+
+                //   ㄹㅇㄹㅁㅇㄴ
+                // </div>
+                // <div className="flex-col cursor-pointer" key={index}>
+
+                //   // Developer
+                //
+              );
+            })}
+        </div>
+      </div>
+
+      {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        >  
           <Image
             aria-hidden
             src="/window.svg"
@@ -80,22 +111,7 @@ export default function Home() {
           />
           Examples
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
