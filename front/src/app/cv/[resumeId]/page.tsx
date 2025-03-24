72d34e181bc60ec733c8a3bd85954eec832585f5
Yumin Kim <49779139+Yumin2019@users.noreply.github.com>
2025-03-24 21:42:39 +0900

    [이력서 수정 페이지] feat: 기술스택 이벤트 처리 추가 feat: 커리어뷰 구조화(작업중)

--- front/src/app/cv/[resumeId]/page.tsx
+++ front/src/app/cv/[resumeId]/page.tsx
@@ -3,44 +3,93 @@
 import { attrBorderGrey, textBlue } from "@/color";
 import { RenderTechstacks } from "@/components/tag-list";
 import { Box, Flex, Spacer, Stack, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { JSX, useCallback, useEffect, useState } from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import { FaCheck, FaPlus } from "react-icons/fa6";
+import { GoDotFill } from "react-icons/go";
+import { IoIosMenu } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
 
+const defaultTechstacks = [
+  "Git",
+  "JavaScript",
+  "Python",
+  "MySQL",
+  "Java",
+  "Linux",
+  "Github",
+  "AWS",
+  "Docker",
+  "HTML",
+  "SQL",
+  "TypeScript",
+  "C++",
+  "React",
+  "Node.js",
+  "C#",
+  "JPA",
+  "Spring Boot",
+  "C / C++",
+];
+
+type Performance = {
+  id: number;
+  title: string;
+  date: string;
+  desc: string;
+};
+
+type CareerData = {
+  date: string;
+  isWorking: false;
+  company: string;
+  department: string;
+  performanceList: Performance[];
+};
+
+let performanceId = 100;
+
 export default function ResumeEditPage() {
-  const [titleText, setTitleText] = useState("개발자 김유민입니다.");
+  // 정보 읿력 관련
+  const [titleText, setTitleText] = useState("");
+  const [nameText, setNameText] = useState("");
+  const [emailText, setEmailText] = useState("");
+  const [phoneText, setPhoneText] = useState("");
+  const [introText, setIntroText] = useState("");
+
+  // 기술스택 관련
   const [isStackOpened, setIsStackOpened] = useState(false);
-  const [selectedTechStacks, setSelectedTechStacks] = useState([]);
-  const defaultTechstacks = [
-    "Git",
-    "JavaScript",
-    "Python",
-    "MySQL",
-    "Java",
-    "Linux",
-    "Github",
-    "AWS",
-    "Docker",
-    "Spring Framework",
-    "HTML",
-    "SQL",
-    "TypeScript",
-    "C++",
-    "React",
-    "Node.js",
-    "C#",
-    "JPA",
-    "Spring Boot",
-    "C / C++",
-  ];
+  const [selectedTechStacks, setSelectedTechStacks] = useState<string[]>([]);
+
+  const [careerDataList, setCareerDataList] = useState<CareerData[]>([
+    {
+      // 작업중...... id로 찾아서 일괄 수정.....
+      date: "2023",
+      isWorking: false,
+      company: "company",
+      department: "department",
+      performanceList: [
+        {
+          id: 0,
+          title: "title",
+          date: "date",
+          desc: "desc",
+        },
+      ],
+    },
+  ]);
 
   const GreenStackView = (v: string, index: number) => {
     const [isSelected, setIsSelected] = useState(false);
 
+    useEffect(() => {
+      let idx = selectedTechStacks.findIndex((s) => s === v);
+      setIsSelected(idx !== -1);
+    }, [selectedTechStacks]);
+
     return (
       <Flex
-        key={index}
+        key={v}
         bg="#e5f4ed"
         borderRadius="20px"
         pl="16px"
@@ -49,20 +98,21 @@ export default function ResumeEditPage() {
         pb="8px"
         fontSize="14px"
         alignItems="center"
+        hidden={isStackOpened ? false : index >= 7}
         border={
           isSelected ? "1px solid #1fba9c" : "1px solid rgba(0, 0, 0, 0.0)"
         }
         cursor="pointer"
         onClick={() => {
           let value = !isSelected;
-          let arr: string[] = [...selectedTechStacks];
           if (value) {
-            arr.push(v);
+            selectedTechStacks.push(v as any);
           } else {
-            arr.splice(index, 1);
+            let deleteIdx = selectedTechStacks.findIndex((s) => s === v);
+            selectedTechStacks.splice(deleteIdx, 1);
           }
 
-          setSelectedTechStacks(arr as any);
+          setSelectedTechStacks([...selectedTechStacks]);
           setIsSelected(value);
         }}
       >
@@ -73,10 +123,10 @@ export default function ResumeEditPage() {
     );
   };
 
-  const StackTagView = (v: string, index: number) => {
+  const StackTagView = (v: string) => {
     return (
       <Flex
-        key={index}
+        key={v}
         bg="#f3f5f8"
         borderRadius="20px"
         pl="16px"
@@ -85,16 +135,184 @@ export default function ResumeEditPage() {
         pb="8px"
         fontSize="14px"
         alignItems="center"
-        cursor="pointer"
       >
         <Text mr="8px">{v}</Text>
         <IoClose
+          cursor="pointer"
           color="grey"
           size="18px"
           onClick={() => {
-            // 삭제 처리
+            let deleteIdx = selectedTechStacks.findIndex((s) => s === v);
+            selectedTechStacks.splice(deleteIdx, 1);
+
+            setSelectedTechStacks([...selectedTechStacks]);
           }}
         />
+      </Flex>
+    );
+  };
+
+  const CareerView = (v: CareerData, index: number) => {
+    const [dateText, setDateText] = useState(v.date);
+    const [isWorking, setIsWorking] = useState<boolean>(v.isWorking);
+    const [companyText, setCompanyText] = useState(v.company);
+    const [departmentText, setDepartmentText] = useState(v.department);
+    const [performanceList, setPerformanceList] = useState(v.performanceList);
+
+    const PerformanceView = (v: any) => {
+      return (
+        <Box className="hover:bg-white" p="24px" key={v.id}>
+          <Flex alignItems="center">
+            <IoIosMenu size="22px" color="grey" />
+            <Box mr="8px" ml="8px">
+              <GoDotFill />
+            </Box>
+            <input
+              placeholder="주요 성과"
+              className="inputWithoutBorder"
+              style={{ fontSize: "16px", width: "100%" }}
+              value={v.title}
+              onChange={(e) => {
+                let editIndex = performanceList.findIndex((e) => e.id === v.id);
+                performanceList[editIndex].title = e.currentTarget.value;
+                setPerformanceList([...performanceList]);
+              }}
+            />
+
+            <Spacer />
+            <IoClose
+              color="grey"
+              size="20px"
+              cursor="pointer"
+              onClick={() => {
+                let deleteIdx = performanceList.findIndex((e) => e.id === v.id);
+                if (deleteIdx !== -1) {
+                  performanceList.splice(deleteIdx, 1);
+                  setPerformanceList([...performanceList]);
+                }
+              }}
+            />
+          </Flex>
+
+          <Box ml="54px" mt="8px">
+            <input
+              placeholder="YYYY.MM - YYYY.MM"
+              className="inputWithoutBorder"
+              style={{ fontSize: "14px", width: "100%" }}
+              // value={perDateText}
+              // onChange={(e) => {
+              //   setPerDateText(e.currentTarget.value);
+              // }}
+            />
+            <input
+              placeholder="상세 업무 내역과 성과를 기입해주세요."
+              className="inputWithoutBorder"
+              style={{ fontSize: "14px", width: "100%" }}
+              // value={descText}
+              // onChange={(e) => {
+              //   setDescText(descText);
+              // }}
+            />
+          </Box>
+        </Box>
+      );
+    };
+
+    return (
+      <Flex
+        key={index}
+        borderBottom={attrBorderGrey}
+        mt="24px"
+        className="hover:bg-gray-50"
+        p="24px"
+      >
+        <Box>
+          <Flex>
+            <Box mr="4px">
+              <IoIosMenu size="22px" color="grey" />
+            </Box>
+            <input
+              placeholder="YYYY.MM - YYYY.MM"
+              className="inputWithoutBorder"
+              style={{ fontSize: "14px", width: "140px" }}
+              value={dateText}
+              onChange={(e) => {
+                setDateText(e.currentTarget.value);
+              }}
+            />
+          </Flex>
+
+          <Flex mt="16px" ml="4px">
+            <input
+              type="checkbox"
+              checked={isWorking}
+              onChange={(e) => {
+                setIsWorking(e.target.checked);
+              }}
+            />
+            <Text
+              ml="8px"
+              fontSize="14px"
+              color="grey"
+              onClick={() => {
+                setIsWorking(!isWorking);
+              }}
+            >
+              현재 재직중
+            </Text>
+          </Flex>
+        </Box>
+
+        <Box ml="80px" w="100%">
+          <input
+            placeholder="회사명"
+            className="inputWithoutBorder"
+            style={{ fontSize: "20px", width: "100%" }}
+            value={companyText}
+            onChange={(e) => {
+              setCompanyText(e.currentTarget.value);
+            }}
+          />
+
+          <Box mt="2px">
+            <input
+              placeholder="부서명/직책"
+              className="inputWithoutBorder"
+              style={{ fontSize: "16px", width: "100%" }}
+              value={departmentText}
+              onChange={(e) => {
+                setDepartmentText(e.currentTarget.value);
+              }}
+            />
+          </Box>
+
+          <Text
+            mt="20px"
+            mb="20px"
+            fontSize="16px"
+            color={textBlue}
+            cursor="pointer"
+            onClick={() => {
+              performanceList.push({
+                id: performanceId++,
+                title: "",
+                date: "",
+                desc: "",
+              });
+              setPerformanceList([...performanceList]);
+            }}
+          >
+            + 주요 성과 추가
+          </Text>
+
+          {/* 성과 리스트 */}
+          {performanceList.map((v: any) => {
+            return PerformanceView(v);
+          })}
+        </Box>
+
+        <Spacer />
+        <IoClose color="grey" size="24px" cursor="pointer" />
       </Flex>
     );
   };
@@ -131,6 +349,10 @@ export default function ResumeEditPage() {
           placeholder="이력서 제목(필수)"
           className="inputWithoutBorder"
           style={{ fontSize: "36px", width: "100%" }}
+          value={titleText}
+          onChange={(e) => {
+            setTitleText(e.currentTarget.value);
+          }}
         />
       </Box>
 
@@ -139,6 +361,10 @@ export default function ResumeEditPage() {
           placeholder="이름(필수)"
           className="inputWithoutBorder"
           style={{ fontSize: "16px", width: "100%" }}
+          value={nameText}
+          onChange={(e) => {
+            setNameText(e.currentTarget.value);
+          }}
         />
 
         <Box mt="8px" />
@@ -147,6 +373,10 @@ export default function ResumeEditPage() {
           placeholder="이메일(필수)"
           className="inputWithoutBorder"
           style={{ fontSize: "16px", width: "100%" }}
+          value={emailText}
+          onChange={(e) => {
+            setEmailText(e.currentTarget.value);
+          }}
         />
 
         <Box mt="8px" />
@@ -155,6 +385,10 @@ export default function ResumeEditPage() {
           placeholder="연락처(필수) ex) 010-0000-0000"
           className="inputWithoutBorder"
           style={{ fontSize: "16px", width: "100%" }}
+          value={phoneText}
+          onChange={(e) => {
+            setPhoneText(e.currentTarget.value);
+          }}
         />
       </Box>
 
@@ -172,6 +406,10 @@ export default function ResumeEditPage() {
           placeholder="간단한 자기소개를 통해 이력서를 돋보이게 만들어보세요. (3~5줄 권장)"
           className="inputWithoutBorder"
           style={{ fontSize: "16px", width: "100%" }}
+          value={introText}
+          onChange={(e) => {
+            setIntroText(e.currentTarget.value);
+          }}
         />
       </Box>
 
@@ -196,6 +434,14 @@ export default function ResumeEditPage() {
         className="hover:bg-gray-50"
         cursor="pointer"
         mt="20px"
+        onClick={() => {
+          // careerViewList.push({
+          //   idx: careerViewIdx,
+          //   element: CareerView(careerViewIdx, deleteItem),
+          // });
+          // ++careerViewIdx;
+          // setCareerViewList([...careerViewList]);
+        }}
       >
         <Spacer />
         <FaPlus size="18px" />
@@ -204,6 +450,11 @@ export default function ResumeEditPage() {
         </Text>
         <Spacer />
       </Flex>
+
+      {/* 커리어 뷰 리스트 */}
+      {careerDataList.map((v, index) => {
+        return CareerView(v, index);
+      })}
 
       {/* 학력 항목 */}
       <Text mt="80px">학력</Text>
@@ -237,9 +488,9 @@ export default function ResumeEditPage() {
         </Text>
       </Box>
 
-      <Box bg="#f5fcf8" p="12px">
+      <Box bg="#f5fcf8" p="12px" mt="20px">
         <Flex>
-          <Text fontSize="12px">
+          <Text fontSize="12px" mb="15px">
             같은 직무의 서류 합격 이력서에서 많이 사용된 스킬입니다. 스킬을
             추가해보세요!
           </Text>
@@ -255,11 +506,80 @@ export default function ResumeEditPage() {
           </Text>
         </Flex>
 
-        <Stack>{RenderTechstacks(defaultTechstacks, 5, GreenStackView)}</Stack>
-        <Stack>{RenderTechstacks(defaultTechstacks, 5, StackTagView)}</Stack>
+        <Stack>{RenderTechstacks(defaultTechstacks, 7, GreenStackView)}</Stack>
       </Box>
 
-      <Box mt="400px" />
+      <Stack mt="16px">
+        {RenderTechstacks(selectedTechStacks, 7, StackTagView)}
+      </Stack>
+
+      {/* 수상 및 기타 */}
+      <Text mt="80px">수상 및 기타</Text>
+      <Box borderTop={attrBorderGrey} mt="8px" />
+      <Box bg="#f3f9fe" pl="12px" pr="12px" pt="8px" pb="8px" mt="8px">
+        <Text whiteSpace="pre-line" fontSize="12px" color="grey">
+          {`• 수상 이력, 직무 관련 자격증, 수료한 교육이나 참석한 외부활동 등이 있다면 간략히 작성해주세요.
+• 지원하는 회사에서 요구하는 경우가 아니라면 운전면허증과 같은 자격증은 생략하는 것이 좋습니다!`}
+        </Text>
+      </Box>
+
+      <Text
+        mt="30px"
+        mb="30px"
+        fontSize="18px"
+        color={textBlue}
+        cursor="pointer"
+        onClick={() => {
+          console.log("추가");
+        }}
+      >
+        + 추가
+      </Text>
+
+      <Text mt="80px">외국어</Text>
+      <Box borderTop={attrBorderGrey} mt="8px" />
+      <Box bg="#f3f9fe" pl="12px" pr="12px" pt="8px" pb="8px" mt="8px">
+        <Text whiteSpace="pre-line" fontSize="12px" color="grey">
+          {`• 외국어 자격증을 보유한 경우 작성해주세요. 
+• 활용 가능한 외국어가 있다면, 어느정도 수준인지 레벨을 선택해주세요.`}
+        </Text>
+      </Box>
+
+      <Text
+        mt="30px"
+        mb="30px"
+        fontSize="18px"
+        color={textBlue}
+        cursor="pointer"
+        onClick={() => {
+          console.log("추가");
+        }}
+      >
+        + 추가
+      </Text>
+
+      <Text mt="80px">외국어</Text>
+      <Box borderTop={attrBorderGrey} mt="8px" />
+      <Box bg="#f3f9fe" pl="12px" pr="12px" pt="8px" pb="8px" mt="8px">
+        <Text whiteSpace="pre-line" fontSize="12px" color="grey">
+          {`• 깃헙, 노션으로 작성한 포트폴리오, 구글 드라이브 파일 등 업무 성과를 보여줄 수 있는 링크가 있다면 작성해주세요.`}
+        </Text>
+      </Box>
+
+      <Text
+        mt="30px"
+        mb="30px"
+        fontSize="18px"
+        color={textBlue}
+        cursor="pointer"
+        onClick={() => {
+          console.log("추가");
+        }}
+      >
+        + 추가
+      </Text>
+
+      <Box mt="700px" />
     </Box>
   );
 }
